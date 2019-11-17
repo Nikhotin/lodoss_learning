dfc6977703c47e1173b26bf9a75a634c6fa537b4
Roman <r.nikhotin@gmail.com>
2019-11-17 17:20:29 +0300

    modified test for course model

--- OOP/tests/test_opp1.js
+++ OOP/tests/test_opp1.js
@@ -4,78 +4,61 @@ const oopTask1 = require('../src/OOP_task1');
 describe("Модель успешно построена", function() {
     
     before(function(){
-        oopTask1.makeCourse("Back-end dev");
-        oopTask1.makeTheme("console");
-        oopTask1.makeQuestion("How can you make new file?");
-        oopTask1.makeAnswer("cd", false);
-        oopTask1.makeAnswer("touch", true);
-        oopTask1.makeAnswer("mkdir", false);
-        oopTask1.makeAnswer("mv", false);
     })
 
     describe("Курс успешно создан", function() {
         
         it("Курс создан", function() {
-            assert.isObject();
-            assert.isString();
-            assert.isArray();
+            assert.isObject(oopTask1.courses);
+            assert.isString(oopTask1.courses.courseName);
+            assert.isArray(oopTask1.courses.theme);
         });
     
     });
 
     describe("Темы успешно добавлены", function() {
         it("Количество тем равно заданному количеству", function() {
-            assert.isObject();
-            assert.isString();
-            assert.isArray();
+            let themeAmount = oopTask1.courses.theme.length;
+            for(themeAmount; themeAmount > 0; themeAmount--){
+                assert.isObject(oopTask1.courses.theme[themeAmount-1]);
+                assert.isString(oopTask1.courses.theme[themeAmount-1].themeName);
+                assert.isArray(oopTask1.courses.theme[themeAmount-1].question);
+            }
+ 
         });
     
     });
 
     describe("Вопросы успешно добавлены", function() {
         it("Количество вопросов равно заданному количеству", function() {
-            assert.isObject();
-            assert.isString();
-            assert.isArray();
+            let themeAmount = oopTask1.courses.theme.length;
+            for(themeAmount; themeAmount > 0; themeAmount--){
+                let questionAmount = oopTask1.courses.theme[themeAmount-1].question.length;
+                for(questionAmount; questionAmount>0; questionAmount--){
+                    assert.isObject(oopTask1.courses.theme[themeAmount-1].question[questionAmount-1]);
+                    assert.isString(oopTask1.courses.theme[themeAmount-1].question[questionAmount-1].questionName);
+                    assert.isArray(oopTask1.courses.theme[themeAmount-1].question[questionAmount-1].answer);
+                }
+            }
         });
     
     });
 
     describe("Ответы успешно добавлены", function() {
-        it("Количество ответов равно заданному количеству", function() {
-            assert.isObject();
-            assert.isString();
-            assert.isArray();
-        });
         it("В каждом вопросе есть хотя бы один правильный ответ", function() {
-            assert.isObject();
-            assert.isString();
-            assert.isArray();
+            let themeAmount = oopTask1.courses.theme.length;
+            for(themeAmount; themeAmount > 0; themeAmount--){
+                let questionAmount = oopTask1.courses.theme[themeAmount-1].question.length;
+                for(questionAmount; questionAmount>0; questionAmount--){
+                    let answerAmount = oopTask1.courses.theme[themeAmount-1].question[questionAmount-1].answer.length
+                    for(answerAmount; answerAmount>0; answerAmount--){
+                        assert.isString(oopTask1.courses.theme[themeAmount-1].question[questionAmount-1].answer[answerAmount-1].answerName);
+                        assert.isBoolean(oopTask1.courses.theme[themeAmount-1].question[questionAmount-1].answer[answerAmount-1].correct);
+                    }       
+                }
+            }    
         });
     
     });
 
 });
-
-
-course = {
-    name: "Back-end dev",
-    theme: [
-        {name: "console", question: [
-            {name: "How can you make new file", answer: [
-                {difin: "cd", correct: false},{difin: "mv", correct: false},
-                {difin: "touch", correct: true},{difin: "mkdir", correct: false}]}, 
-            {name: "Question2", answer: []}, 
-            {name: "Question3", answer: []}, 
-            {name: "Question4", answer: []}]}, 
-        {name: "GitHub", question: [  
-            {name: "Question1", answer: []}, {name: "Question2", answer: []}, 
-            {name: "Question3", answer: []}, {name: "Question4", answer: []}]}, 
-        {name: "JS", question: [
-            {name: "Question1", answer: []}, {name: "Question2", answer: []}, 
-            {name: "Question3", answer: []}, {name: "Question4", answer: []}]},
-        {name: "OOP", question: [
-            {name: "Question1", answer: []}, {name: "Question2", answer: []}, 
-            {name: "Question3", answer: []}, {name: "Question4", answer: []}]}
-    ]
-}
