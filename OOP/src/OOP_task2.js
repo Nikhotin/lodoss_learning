2649713622314ac39bbf4a107a9e80fce5ae8afd
Nikhotin Roman <r.nikhotin@gmail.com>
2019-11-25 15:49:00 +0300

    Переписал тесты для задания 2 ООП + с нуля переписал само задание

--- OOP/src/OOP_task2.js
+++ OOP/src/OOP_task2.js
@@ -1,96 +1,201 @@
-function randIntMixin(min, max) {
+function randInt(min, max) {
     let rand = min + Math.random() * (max + 1 - min);
     return Math.floor(rand);
   }
 
-class Manufacturer{
+
+class Producer{
     constructor(){
-        this.produce = randIntMixin(50,150);
+        this.product = [];
+        this.left = [];
     }
 
+    produceProduct(min,max){
+        let i = this.product.length - 1;
+        if(this.left[i] != undefined){
+            this.product.push(randInt(min,max) + this.left[i]);
+        }else{
+            this.product.push(randInt(min,max));
+        }
+    }
 }
 
-class Intermediary extends Manufacturer{
+
+class Deliveryman{
     constructor(){
-        super();
+        this.productBought = [];
+        this.left = [];
+    }
 
-        if (this.produce <= 100){
-            this.leftManufact = 0;
+    giveLeftProductForProducer(producer){
+        let i = producer.product.length - 1;
+        if(producer.product[i] <= 100){
+            producer.left.push(0);
         }else{
-            this.leftManufact = this.produce - 100;
+            producer.left.push(producer.product[i] - 100);
         }
+    }
 
-        if (this.produce >= 100){
-            this.deliver = 100;
+    getProduct(producer){
+        let i = producer.product.length - 1;
+        if(producer.product[i] + this.left[i-1] >= 100 && this.left[i-1] != undefined){
+            this.productBought.push(100);
+            producer.left[i] = producer.product[i] + this.left[i-1] - 100;
+        }else if(producer.product[i] + this.left[i-1] < 100 && this.left[i-1] != undefined){
+            this.productBought.push(producer.product[i]);
+        }else if(producer.product[i] >= 100){
+            this.productBought.push(100);
+        }else{
+            this.productBought.push(producer.product[i]);
+        }
+    }
+}
+
+
+class Customer{
+    constructor(){
+        this.needs = [];
+        this.purchased = [];
+        this.unmetNeeds = [];
+    }
+
+    generateNeeds(min,max){
+        this.needs.push(randInt(min,max));
+    }
+
+    giveLeftProductForDeliver(deliver){
+        let i = producer.product.length - 1;
+        if(deliver.productBought[i] > this.needs[i]){
+            deliver.left.push(deliver.productBought[i] - this.needs[i]);
+        }else{
+            deliver.left.push(0);
+        }
+    }
+
+    buyProduct(deliver){
+        let i = producer.product.length - 1;
+        if (this.needs[i] >= deliver.productBought[i]){
+            this.purchased.push(deliver.productBought[i]);
         } else{
-            this.deliver = this.produce;
+            this.purchased.push(this.needs[i]);
         }
-    }   
-}
+    }
 
-class Customer extends Intermediary{
-    constructor(){
-        super();
-        this.needs = randIntMixin(70,120);
-
-        if(this.deliver > this.needs){
-            this.leftDeliv = this.deliver - this.needs;
+    determineSatisfaction(deliver){
+        let i = producer.product.length - 1;
+        if(this.needs[i] > deliver.productBought[i]){
+            this.unmetNeeds.push(false);
         }else{
-            this.leftDeliv = 0;
-        }
-
-        if (this.needs >= this.deliver){
-            this.purchased = this.deliver;
-        } else{
-            this.purchased = this.needs;
-        }
-
-        if(this.needs > this.deliver){
-            this.unmetNeeds = false;
-        }else{
-            this.unmetNeeds = true;
+            this.unmetNeeds.push(true);
         }
     }
 }
 
-class Product extends Customer{
+class Statistic{
     constructor(){
-        super();
     }
-    countTotalProduce(){
 
-    }
-    countTotalNeeds(){
-
-    }
-    countMeanDeliver(){
-
-    }
-    countProduceLast3Days(){
-
-    }
-    countDeliverLast3Days(){
-
-    }
-    countKPIofInterm(){
-        
-    }
-    simulateMovement(daysAmount){
-        let daysArr = [];
-        for(daysAmount; daysAmount>0;daysAmount--){
-            let day = new Product;
-            daysArr.push(day)
+    simulateMovement(producer, deliveryman, customer, daysAmount){
+        for(daysAmount; daysAmount>0; daysAmount--){
+            producer.produceProduct(50,150);
+            deliveryman.giveLeftProductForProducer(producer);
+            deliveryman.getProduct(producer);
+            customer.generateNeeds(70,120);
+            customer.giveLeftProductForDeliver(deliveryman);
+            customer.buyProduct(deliveryman);
+            customer.determineSatisfaction(deliveryman);
         }
-        // daysAmount = daysArr.length - 1;
-        // for(daysAmount; daysAmount>0; daysAmount--){
-        //     daysArr[daysAmount-1].produce += daysArr[daysAmount].leftManufact;
-        // }
-        return daysArr
+    }
+
+    calculateTotalProduce(producer){
+        this.totalProduce = 0;
+        if(producer.left[producer.left.length-1] != undefined)
+        this.totalProduce = producer.left[producer.left.length-1];
+    }
+
+    calculateTotalNeeds(customer){
+        let totalNeeds = 0;
+        let i = customer.needs.length;
+        for(i; i>0; i--){
+            totalNeeds += customer.needs[i-1];
+        }
+
+        this.totalNeeds = totalNeeds;
+    }
+
+    calculateMeanDeliver(deliver){
+        let totalDeliv = 0;
+        let i = deliver.productBought.length;
+        for(i; i>0; i--){
+            totalDeliv += deliver.productBought[i-1];
+        }
+
+        this.meanDeliver = totalDeliv/deliver.productBought.length;
+    }
+
+    calculateProduceLast3Days(producer){
+        let produceLast3Days = 0;
+        let arrLast3Days = producer.product.slice(-3);
+        let i = arrLast3Days.length - 1;
+        for(i; i>=0; i--){
+            produceLast3Days += arrLast3Days[i];
+        }
+
+        this.produceLast3Days = produceLast3Days;
+    }
+
+    calculateDeliverLast3Days(deliver){
+        let deliverLast3Days = 0;
+        let arrLast3Days = deliver.productBought.slice(-3);
+        let i = arrLast3Days.length - 1;
+        for(i; i>=0; i--){
+            deliverLast3Days += arrLast3Days[i];
+        }
+
+        this.deliverLast3Days = deliverLast3Days;
+    }
+
+    calculateKpiDeliverman(deliver){
+        if(deliver.productBought == undefined || deliver.productBought == 0){
+            this.kpiDeliverman = 0 + '%';
+            return
+        }
+
+        let totalProductBought = 0;
+        let totalLeft = 0;
+        let i = deliver.productBought.length;
+        for(i; i>0; i--){
+            totalProductBought += deliver.productBought[i-1];
+        }
+        i = deliver.left.length;
+        for(i; i>0; i--){
+            totalLeft += deliver.left[i-1];
+        }
+
+        this.kpiDeliverman = Math.floor(((totalProductBought - totalLeft)/totalProductBought)*100) + '%';
     }
 }
 
-let product = new Product;
-product = product.simulateMovement(10);
-console.table(product)
+let producer = new Producer();
+let deliveryman = new Deliveryman();
+let customer = new Customer();
+let statistic = new Statistic();
+statistic.simulateMovement(producer, deliveryman, customer, 10);
+statistic.calculateTotalProduce(producer);
+statistic.calculateTotalNeeds(customer);
+statistic.calculateMeanDeliver(deliveryman);
+statistic.calculateProduceLast3Days(producer);
+statistic.calculateDeliverLast3Days(deliveryman);
+statistic.calculateKpiDeliverman(deliveryman);
 
-module.exports = product;
+console.table(producer)
+console.table(deliveryman)
+console.table(customer)
+console.table(statistic)
+
+module.exports = {
+    producer,
+    deliveryman,
+    customer,
+    statistic
+};
--- OOP/tests/test_oop2.js
+++ OOP/tests/test_oop2.js
@@ -3,150 +3,134 @@ const oopTask2 = require('../src/OOP_task2');
 
 describe("Движение товара за 10 дней успешно смоделированно", function() {
 
-    describe("Модель движения товара за 1 день построена", function() {
-        
-        it("Количество произведенного товара задано", function() {
-            let amount = oopTask2.length;
-            for(amount; amount > 0; amount--){
-                assert.isNotNull(oopTask2[amount-1].produce);
-                assert.isAtMost(oopTask2[amount-1].produce, 150);
-                assert.isAtLeast(oopTask2[amount-1].produce, 50);
-            }
-        });
-
-        it("Количество доставленного товара задано", function() {
-            let amount = oopTask2.length;
-            for(amount; amount > 0; amount--){
-                assert.isNotNull(oopTask2[amount-1].deliver);
-                assert.isAtMost(oopTask2[amount-1].deliver, 100);
-            }
-        });
-
-        it("Необходимость в товаре задана", function() {
-            let amount = oopTask2.length;
-            for(amount; amount > 0; amount--){
-                assert.isNotNull(oopTask2[amount-1].needs);
-                assert.isAtMost(oopTask2[amount-1].needs, 120);
-                assert.isAtLeast(oopTask2[amount-1].needs, 70);
-            }
-        });
-
-        it("Количество купленного товара задано", function() {
-            let amount = oopTask2.length;
-            for(amount; amount > 0; amount--){
-                assert.isNotNull(oopTask2[amount-1].purchased);
-                assert.isAtMost(oopTask2[amount-1].purchased, 120);
-                assert.isAtLeast(oopTask2[amount-1].purchased, 50);
-            }
-        });
-    
-        it("Дельта между произведенным и доставленным товаром посчитанна верно", function() {
-            let amount = oopTask2.length;
-            for(amount; amount > 0; amount--){
-                let delta = oopTask2[amount-1].produce - oopTask2[amount-1].deliver;
-                if (delta < 0) delta = 0;
-                assert.equal(oopTask2[amount-1].leftManufact, delta);
-            }
-        });
-
-        it("Дельта между доставленным и купленным товаром посчитанна верно", function() {
-            let amount = oopTask2.length;
-            for(amount; amount > 0; amount--){
-                let delta = oopTask2[amount-1].deliver - oopTask2[amount-1].needs;
-                if (delta < 0) delta = 0;
-                assert.equal(oopTask2[amount-1].leftDeliv, delta);
-            }
-        });
-        
-    });
-
     describe("Количество товара у производителя по истечению 10 дней - верно", function() {
         
-        it("Оно = сумме кол-в товара за каждый день", function() {
-            let amount = oopTask2.length;
-            for(amount; amount > 0; amount--){
-                let total = 0;
-                let arrProduct = oopTask2.slice(0, amount-1);
-                arrProduct.forEach(function(elem){
-                    total += elem.produce
-                    return total
-                })
-                assert.equal(oopTask2.totalProduce, total);
-            }
+        it("Метод calculateTotalProduce определяет последний элемент массива оставшихся у производителя товаров", function() {
+            oopTask2.statistic.calculateTotalProduce({left: [1,2,3,4,5,6]});
+            assert.equal(oopTask2.statistic.totalProduce, 6);
+
+            oopTask2.statistic.calculateTotalProduce({left: []});
+            assert.equal(oopTask2.statistic.totalProduce, 0);
+
+            oopTask2.statistic.calculateTotalProduce({left: [1,2,3,4,5,10000]});
+            assert.equal(oopTask2.statistic.totalProduce, 10000);
+
+            oopTask2.statistic.calculateTotalProduce({left: [1]});
+            assert.equal(oopTask2.statistic.totalProduce, 1);
         });
     
     });
     
     describe("Количество товара необходимого потребителю по истечению 10 дней - верно", function() {
         
-        it("Оно = сумме кол-в товара необходимого потребителю за каждый день", function() {
-            let amount = oopTask2.length;
-            for(amount; amount > 0; amount--){
-                let total = 0;
-                let arrProduct = oopTask2.slice(0, amount-1);
-                arrProduct.forEach(function(elem){
-                    total += elem.needs
-                    return total
-                })
-                assert.equal(oopTask2.totalNeeds, total);
-            }
+        it("Метод calculateTotalNeeds вычисляет сумму элементов массива потребностей покупателя", function() {
+            oopTask2.statistic.calculateTotalNeeds({needs: [1,1,1,1,1,1]});
+            assert.equal(oopTask2.statistic.totalNeeds, 6);
+
+            oopTask2.statistic.calculateTotalNeeds({needs: [-1000,500,500]});
+            assert.equal(oopTask2.statistic.totalNeeds, 0);
+
+            oopTask2.statistic.calculateTotalNeeds({needs: []});
+            assert.equal(oopTask2.statistic.totalNeeds, 0);
+
+            oopTask2.statistic.calculateTotalNeeds({needs: [1000,2000,3000,4]});
+            assert.equal(oopTask2.statistic.totalNeeds, 6004);
         });
     
     });
 
     describe("Среднее количество доставленного товара за день - верно", function() {
         
-        it("Оно = всего доставленно товара/кол-во дней", function() {
-            let amount = oopTask2.length;
-            for(amount; amount > 0; amount--){
-                let arrProduct = oopTask2.slice(0, amount-1);
-                let total = oopTask2[amount-1].totalDeliver/arrProduct.length;
-                assert.equal(oopTask2[amount-1].meanDeliver, total);
-            }
+        it("Метод calculateMeanDeliver вычисляет среднее значение массива купленных у производителя товаров", function() {
+            oopTask2.statistic.calculateMeanDeliver({productBought:[4,4,4,4,4,4,4,4,4]});
+            assert.equal(oopTask2.statistic.meanDeliver, 4);
+
+            oopTask2.statistic.calculateMeanDeliver({productBought:[]});
+            assert.isNaN(oopTask2.statistic.meanDeliver);
+
+            oopTask2.statistic.calculateMeanDeliver({productBought:[0]});
+            assert.equal(oopTask2.statistic.meanDeliver, 0);
+
+            oopTask2.statistic.calculateMeanDeliver({productBought:[1000,2000,1000,2000,1000,2000]});
+            assert.equal(oopTask2.statistic.meanDeliver, 1500);
         });
     
     });
 
     describe("Количество товара произведенного за последние 3 дня - верно", function() {
         
-        it("Оно = произведено товара за последние 3 дня/3", function() {
-            let amount = oopTask2.length;
-            for(amount; amount > 0; amount--){
-                let arrProduct = oopTask2.slice(amount-4, amount-1);
-                let total = 0;
-                arrProduct.forEach(function(elem){
-                    total += elem.produce
-                });
-                assert.equal(oopTask2.produceLast3Days, total);
-            }
+        it("Метод calculateProduceLast3Days вычисляет сумму 3х последних элементов массива произведенной продукции", function() {
+            oopTask2.statistic.calculateProduceLast3Days({product: [1,2,3,4,5,6]});
+            assert.equal(oopTask2.statistic.produceLast3Days, 15);
+
+            oopTask2.statistic.calculateProduceLast3Days({product: [1,2]});
+            assert.equal(oopTask2.statistic.produceLast3Days, 3);
+
+            oopTask2.statistic.calculateProduceLast3Days({product: [10]});
+            assert.equal(oopTask2.statistic.produceLast3Days, 10);
+
+            oopTask2.statistic.calculateProduceLast3Days({product: []});
+            assert.equal(oopTask2.statistic.produceLast3Days, 0);
         });
     
     });
 
     describe("Количество товара доставленного за последние 3 дня - верно", function() {
         
-        it("Оно = доставленно товара за последние 3 дня/3", function() {
-            let amount = oopTask2.length;
-            for(amount; amount > 0; amount--){
-                let arrProduct = oopTask2.slice(amount-4, amount-1);
-                let total = 0;
-                arrProduct.forEach(function(elem){
-                    total += elem.deliver
-                });
-                assert.equal(oopTask2.deliverLast3Days, total);
-            }
+        it("Метод calculateDeliverLast3Days вычисляет сумму 3х последних элементов массива доставленной продукции", function() {
+            oopTask2.statistic.calculateDeliverLast3Days({productBought: [80,120,30,1,49,150]});
+            assert.equal(oopTask2.statistic.deliverLast3Days, 200);
+
+            oopTask2.statistic.calculateDeliverLast3Days({productBought: [1,2]});
+            assert.equal(oopTask2.statistic.deliverLast3Days, 3);
+
+            oopTask2.statistic.calculateDeliverLast3Days({productBought: [10]});
+            assert.equal(oopTask2.statistic.deliverLast3Days, 10);
+
+            oopTask2.statistic.calculateDeliverLast3Days({productBought: []});
+            assert.equal(oopTask2.statistic.deliverLast3Days, 0);
         });
     
     });
 
     describe("КПД посредника - верно", function() {
         
-        it("КПД = (Кол-во доставленного товара/Кол-во купленного товара)", function() {
-            let amount = oopTask2.length;
-            for(amount; amount > 0; amount--){
-                let kpi = oopTask2[amount-1].allPurchased/oopTask2[amount-1].allDeliver;
-                assert.equal(oopTask2[amount-1].kpi, kpi);
-            }
+        it("Метод calculateKpiDeliverman извлекает целую часть из частного проданной продукции на привезенную продукцию", function() {
+            oopTask2.statistic.calculateKpiDeliverman({
+                productBought: [1,1,1,1,1,1,1,1,1,1],
+                left: [2,0,0,0,0,0,0,0,0,0]
+            });
+            assert.equal(oopTask2.statistic.kpiDeliverman, '80%');
+
+            oopTask2.statistic.calculateKpiDeliverman({
+                productBought: [1,1,1,1,1,1,1,1,1,1],
+                left: [2,0,1,0,1]
+            });
+            assert.equal(oopTask2.statistic.kpiDeliverman, '60%');
+
+            oopTask2.statistic.calculateKpiDeliverman({
+                productBought: [],
+                left: []
+            });
+            assert.equal(oopTask2.statistic.kpiDeliverman, '0%');
+
+            oopTask2.statistic.calculateKpiDeliverman({
+                productBought: [1,1,1],
+                left: [2,0,0,0,0,0,0,0,0,0]
+            });
+            assert.equal(oopTask2.statistic.kpiDeliverman, '33%');
+
+            oopTask2.statistic.calculateKpiDeliverman({
+                productBought: [0],
+                left: [1,3,4,100]
+            });
+            assert.equal(oopTask2.statistic.kpiDeliverman, '0%');
+
+            oopTask2.statistic.calculateKpiDeliverman({
+                productBought: [1,1,1],
+                left: [0]
+            });
+            assert.equal(oopTask2.statistic.kpiDeliverman, '100%');
         });
     
     });
