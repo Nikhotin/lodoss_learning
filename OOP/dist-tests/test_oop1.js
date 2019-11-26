9d4183de36ef77c932c05b047e180cc46cc4bd63
Nikhotin Roman <r.nikhotin@gmail.com>
2019-11-26 12:42:19 +0300

    refactory tests with eslint and add tests in es5

--- OOP/dist-tests/test_oop1.js
+++ OOP/dist-tests/test_oop1.js
@@ -1,19 +1,23 @@
-/* eslint-disable max-len */
-const { assert } = require('chai');
-const oopTask1 = require('../src/OOP_task1');
+'use strict';
 
-describe('Модель успешно построена', () => {
-  describe('Курс успешно создан', () => {
-    it('Курс создан', () => {
+/* eslint-disable max-len */
+var _require = require('chai'),
+    assert = _require.assert;
+
+var oopTask1 = require('../src/OOP_task1');
+
+describe('Модель успешно построена', function () {
+  describe('Курс успешно создан', function () {
+    it('Курс создан', function () {
       assert.isObject(oopTask1);
       assert.isString(oopTask1.courseName);
       assert.isArray(oopTask1.theme);
     });
   });
 
-  describe('Темы успешно добавлены', () => {
-    it('Количество тем равно заданному количеству', () => {
-      let themeAmount = oopTask1.theme.length;
+  describe('Темы успешно добавлены', function () {
+    it('Количество тем равно заданному количеству', function () {
+      var themeAmount = oopTask1.theme.length;
       for (themeAmount; themeAmount > 0; themeAmount -= 1) {
         assert.isObject(oopTask1.theme[themeAmount - 1]);
         assert.isString(oopTask1.theme[themeAmount - 1].themeName);
@@ -22,11 +26,11 @@ describe('Модель успешно построена', () => {
     });
   });
 
-  describe('Вопросы успешно добавлены', () => {
-    it('Количество вопросов равно заданному количеству', () => {
-      let themeAmount = oopTask1.theme.length;
+  describe('Вопросы успешно добавлены', function () {
+    it('Количество вопросов равно заданному количеству', function () {
+      var themeAmount = oopTask1.theme.length;
       for (themeAmount; themeAmount > 0; themeAmount -= 1) {
-        let questionAmount = oopTask1.theme[themeAmount - 1].question.length;
+        var questionAmount = oopTask1.theme[themeAmount - 1].question.length;
         for (questionAmount; questionAmount > 0; questionAmount -= 1) {
           assert.isObject(oopTask1.theme[themeAmount - 1].question[questionAmount - 1]);
           assert.isString(oopTask1.theme[themeAmount - 1].question[questionAmount - 1].questionName);
@@ -36,13 +40,13 @@ describe('Модель успешно построена', () => {
     });
   });
 
-  describe('Ответы успешно добавлены', () => {
-    it('В каждом вопросе есть хотя бы один правильный ответ', () => {
-      let themeAmount = oopTask1.theme.length;
+  describe('Ответы успешно добавлены', function () {
+    it('В каждом вопросе есть хотя бы один правильный ответ', function () {
+      var themeAmount = oopTask1.theme.length;
       for (themeAmount; themeAmount > 0; themeAmount -= 1) {
-        let questionAmount = oopTask1.theme[themeAmount - 1].question.length;
+        var questionAmount = oopTask1.theme[themeAmount - 1].question.length;
         for (questionAmount; questionAmount > 0; questionAmount -= 1) {
-          let answerAmount = oopTask1.theme[themeAmount - 1].question[questionAmount - 1].answer.length;
+          var answerAmount = oopTask1.theme[themeAmount - 1].question[questionAmount - 1].answer.length;
           for (answerAmount; answerAmount > 0; answerAmount -= 1) {
             assert.isString(oopTask1.theme[themeAmount - 1].question[questionAmount - 1].answer[answerAmount - 1].answerName);
             assert.isBoolean(oopTask1.theme[themeAmount - 1].question[questionAmount - 1].answer[answerAmount - 1].correct);
