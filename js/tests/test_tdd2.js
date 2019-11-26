34fb0142a16ece08577f5d8db22543285b6f4cb0
Nikhotin Roman <r.nikhotin@gmail.com>
2019-11-26 12:26:50 +0300

    refactory js_sync with eslint

--- js/tests/test_tdd2.js
+++ js/tests/test_tdd2.js
@@ -1,35 +1,32 @@
-const assert = require('chai').assert;
+const { assert } = require('chai');
 const tddTask2 = require('../src/tdd_task2');
 
 
-describe("catFactory", function() {
-
-    describe("Принадлежность свойств объекта к элементам словаря", function() {
-
-        it("Свойство catName принадлежит массиву name", function() {
-            let a = tddTask2.catFactory();
-            assert.include(tddTask2.name, a.name);
-        });
-        it("Свойство catAge принадлежит массиву age", function() {
-            let a = tddTask2.catFactory();
-            assert.include(tddTask2.age, a.age);
-        });
-        it("Свойство catGender принадлежит массиву gender", function() {
-            let a = tddTask2.catFactory();
-            assert.include(tddTask2.gender, a.gender);
-        });
-        it("Свойство catLegsCount принадлежит массиву legsCount", function() {
-            let a = tddTask2.catFactory();
-            assert.include(tddTask2.legsCount, a.legsCount);
-        });
-        it("Свойство catTailLength принадлежит массиву tailLength", function() {
-            let a = tddTask2.catFactory();
-            assert.include(tddTask2.tailLength, a.tailLength);
-        });
-        it("Свойство catLoudness принадлежит массиву loudness", function() {
-            let a = tddTask2.catFactory();
-            assert.include(tddTask2.loudness, a.loudness);
-        });
-
+describe('catFactory', () => {
+  describe('Принадлежность свойств объекта к элементам словаря', () => {
+    it('Свойство catName принадлежит массиву name', () => {
+      const a = tddTask2.catFactory();
+      assert.include(tddTask2.name, a.name);
     });
+    it('Свойство catAge принадлежит массиву age', () => {
+      const a = tddTask2.catFactory();
+      assert.include(tddTask2.age, a.age);
+    });
+    it('Свойство catGender принадлежит массиву gender', () => {
+      const a = tddTask2.catFactory();
+      assert.include(tddTask2.gender, a.gender);
+    });
+    it('Свойство catLegsCount принадлежит массиву legsCount', () => {
+      const a = tddTask2.catFactory();
+      assert.include(tddTask2.legsCount, a.legsCount);
+    });
+    it('Свойство catTailLength принадлежит массиву tailLength', () => {
+      const a = tddTask2.catFactory();
+      assert.include(tddTask2.tailLength, a.tailLength);
+    });
+    it('Свойство catLoudness принадлежит массиву loudness', () => {
+      const a = tddTask2.catFactory();
+      assert.include(tddTask2.loudness, a.loudness);
+    });
+  });
 });
