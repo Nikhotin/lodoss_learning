ddbdcdf633a5c54962d51ceeb900a95c946e1acc
Nikhotin Roman <r.nikhotin@gmail.com>
2019-12-05 15:01:39 +0300

    добавил 5 новых классов (Фирма, Департамент, Разработчик, Проект и Мусорка). У каждой Фирмы есть директор, список Депортаментов и полученные от заказчиков проекты. У каждого Депортамента есть тип, список проектов и список разработчиков этого Депортамента. Каждый разработчик имеет тип, проект, опыт и количество дней проведенных без проекта. У проекта есть тип, его сложность, количество дней до конца разработки и метрики сделан и протестирован. Класс мусорка будет хранить в себе реализованные проекты и уволенных сотрудников

--- OOP_Task3/src/OOP_task3.js
+++ OOP_Task3/src/OOP_task3.js
@@ -2,3 +2,44 @@ function randInt(min, max) {
   const rand = min + Math.random() * (max + 1 - min);
   return Math.floor(rand);
 }
+class Firm {
+  constructor(directorName, departments = [], projects = []) {
+    this.director = directorName;
+    this.departments = departments;
+    this.projects = projects;
+  }
+}
+
+class Department {
+  constructor(type, projects = [], staff = []) {
+    this.type = type;
+    this.projects = projects;
+    this.staff = staff;
+  }
+}
+
+class Developer {
+  constructor(type, project = null, expirience = 0, daysWithOutProject = 0) {
+    this.type = type;
+    this.project = project;
+    this.expirience = expirience;
+    this.daysWithOutProject = daysWithOutProject;
+  }
+}
+
+class Project {
+  constructor(type, difficulty, made = false, tested = false) {
+    this.type = type;
+    this.difficulty = difficulty;
+    this.daysToProduction = this.difficulty;
+    this.made = made;
+    this.tested = tested;
+  }
+}
+
+class Trash {
+  constructor(projects = [], staff = []) {
+    this.projects = projects;
+    this.staff = staff;
+  }
+}
