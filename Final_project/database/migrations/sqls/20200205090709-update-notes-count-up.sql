e515a943a6d18e3ed56f3c7814cfab45172b0de0
Nikhotin Roman <r.nikhotin@gmail.com>
2020-02-05 13:58:18 +0300

    добавил папку для бд, в которой созданы миграции, с помощью db-migrate

---
+++ Final_project/database/migrations/sqls/20200205090709-update-notes-count-up.sql
@@ -0,0 +1,20 @@
+
+CREATE OR REPLACE FUNCTION updateNotesCount() RETURNS TRIGGER AS $$
+DECLARE
+	usrid int;
+BEGIN
+	IF 	TG_OP = 'INSERT' THEN
+		usrid = NEW.user_id;
+		UPDATE Users SET notes_count = notes_count + 1 WHERE id = usrid;
+		RETURN NEW;
+	ELSIF TG_OP = 'DELETE' THEN
+		usrid = OLD.user_id;
+		UPDATE Users SET notes_count = notes_count - 1 WHERE id = usrid;
+		RETURN OLD;
+	END IF;
+END;
+$$ LANGUAGE plpgsql;
+	
+
+CREATE TRIGGER update_notes_count
+AFTER INSERT OR DELETE ON Notes FOR EACH ROW EXECUTE PROCEDURE updateNotesCount();
---
+++ Final_project/database/migrations/sqls/20200205090800-update-tags-count-up.sql
@@ -0,0 +1,19 @@
+CREATE OR REPLACE FUNCTION updateTagsCount() RETURNS TRIGGER AS $$
+DECLARE
+	ntid int;
+BEGIN
+	IF 	TG_OP = 'INSERT' THEN
+		ntid = NEW.note_id;
+		UPDATE Notes SET tags_count = tags_count + 1 WHERE id = ntid;
+		RETURN NEW;
+	ELSIF TG_OP = 'DELETE' THEN
+		ntid = OLD.note_id;
+		UPDATE Notes SET tags_count = tags_count - 1 WHERE id = ntid;
+		RETURN OLD;
+	END IF;
+END;
+$$ LANGUAGE plpgsql;
+	
+
+CREATE TRIGGER update_tags_count
+AFTER INSERT OR DELETE ON Hashtags FOR EACH ROW EXECUTE PROCEDURE updateTagsCount();
